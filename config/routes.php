37ec389ba733bbe17bb20af597b479b8a0d4a04b
lbob <skyweo@foxmail.com>
2014-11-23 20:31:09 +0800

    排除目录 config

--- config/routes.php
+++ config/routes.php
@@ -43,14 +43,14 @@ $routes->bindFilter('api', array('before' => 'auth|uavAuth'));
 
 
 
-//$routes->filter('auth', function() {
-//    var_dump('auth');
-//    var_dump(func_get_arg(0));
-//    $params = func_get_arg(0);
-//    if (array_key_exists('url', $params) && isset($params['url'])) {
-//        header('location:http://'.$params['url']);
-//    }
-//});
+$routes->filter('auth', function() {
+    var_dump('auth');
+    var_dump(func_get_arg(0));
+    $params = func_get_arg(0);
+    if (array_key_exists('url', $params) && isset($params['url'])) {
+        header('location:http://'.$params['url']);
+    }
+});
 
 
 $routes->missing(function() { echo '<p>Routing missed;</p>'; });
