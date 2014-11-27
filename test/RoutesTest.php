4c6ab9832bd419c7c1cb8b9a199e47fc4d95eb4a
lbob <skyweo@foxmail.com>
2014-11-27 10:13:09 +0800

    创建 Routes 核心类

---
+++ test/RoutesTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class RoutesTest
+ *
+ * @author lbob created at 2014/11/27 10:10
+ */
+class RoutesTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @var Nebula\Routes;
+     */
+    protected $routes;
+}
+
+ 
